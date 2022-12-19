d3ad682c15083c645db89c0b9f7b72b214d1dd6a
marszm <mariusz.szmer@gmail.com>
2022-12-19 18:12:34 +0100

    fixed n+1 problem for Product in ProductController

--- src/main/java/com/example/shop/product/controller/ProductController.java
+++ src/main/java/com/example/shop/product/controller/ProductController.java
@@ -1,16 +1,20 @@
 package com.example.shop.product.controller;
 
+import com.example.shop.product.dto.ProductListDto;
 import com.example.shop.product.model.Product;
 import com.example.shop.product.service.ProductService;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +23,20 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public Page<Product> getProducts(Pageable pageable) {
-        return productService.getProducts(pageable);
+    public Page<ProductListDto> getProducts(Pageable pageable) {
+        Page<Product> products = productService.getProducts(pageable);
+        List<ProductListDto> productListDtos = products.getContent().stream()
+                .map(product -> ProductListDto.builder()
+                        .id(product.getId())
+                        .name(product.getName())
+                        .description(product.getDescription())
+                        .price(product.getPrice())
+                        .currency(product.getCurrency())
+                        .image(product.getImage())
+                        .slug(product.getSlug())
+                        .build())
+                .toList();
+        return new PageImpl<>(productListDtos, pageable, products.getTotalElements());
     }
 
     @GetMapping("/products/{slug}")
